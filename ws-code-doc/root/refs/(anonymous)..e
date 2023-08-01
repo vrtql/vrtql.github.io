1f812d4fb9f20a93ab3d031f1cf9e60acec9523e
vrtql <vrtql@vrtql.com>
2023-08-01 13:14:47 -0500

    update

--- ws-code-doc/root/refs/(anonymous)..e
+++ ws-code-doc/root/refs/(anonymous)..e
@@ -1,6 +1,12 @@
-<dec f='codebrowser/src/vws.h' l='233' type='vws_error_value'/>
+<dec f='codebrowser/src/vws.h' l='235' type='vws_error_value'/>
 <offset>704</offset>
-<doc f='codebrowser/src/vws.h' l='233'>/**&lt; Last error value            */</doc>
+<doc f='codebrowser/src/vws.h' l='235'>/**&lt; Last error value            */</doc>
+<use f='codebrowser/src/rpc.c' l='95' u='m' c='vrtql_rpc_invoke'/>
+<use f='codebrowser/src/rpc.c' l='136' u='m' c='vrtql_rpc_exec'/>
+<use f='codebrowser/src/rpc.c' l='148' u='m' c='vrtql_rpc_exec'/>
+<use f='codebrowser/src/rpc.c' l='186' u='m' c='vrtql_rpc_exec'/>
+<use f='codebrowser/src/rpc.c' l='193' u='m' c='vrtql_rpc_exec'/>
+<use f='codebrowser/src/rpc.c' l='198' u='m' c='vrtql_rpc_exec'/>
 <use f='codebrowser/src/test/test_example.c' l='31' u='m' c='main'/>
 <use f='codebrowser/src/vws.c' l='309' u='m' c='vws_set_error'/>
 <use f='codebrowser/src/vws.c' l='311' u='m' c='vws_set_error'/>
@@ -8,6 +14,6 @@
 <use f='codebrowser/src/vws.c' l='315' u='m' c='vws_set_error'/>
 <use f='codebrowser/src/vws.c' l='319' u='m' c='vws_set_error'/>
 <use f='codebrowser/src/vws.c' l='326' u='r' c='vws_get_error'/>
-<use f='codebrowser/src/vws.c' l='459' u='w'/>
+<use f='codebrowser/src/vws.c' l='457' u='w'/>
 <use f='codebrowser/src/websocket.c' l='440' u='m' c='socket_handshake'/>
 <use f='codebrowser/src/websocket.c' l='476' u='m' c='socket_handshake'/>
--- ws-code-doc/root/refs/ctest_test_data..c
+++ ws-code-doc/root/refs/ctest_test_data..c
@@ -1,14 +1,21 @@
-<dec f='codebrowser/src/test/test_websocket.c' l='32' type='vws_cnx *'/>
-<use f='codebrowser/src/test/test_websocket.c' l='37' u='w' c='ctest_test_setup'/>
-<use f='codebrowser/src/test/test_websocket.c' l='38' u='r' c='ctest_test_setup'/>
-<use f='codebrowser/src/test/test_websocket.c' l='39' u='r' c='ctest_test_setup'/>
-<use f='codebrowser/src/test/test_websocket.c' l='47' u='r' c='ctest_test_teardown'/>
-<use f='codebrowser/src/test/test_websocket.c' l='52' u='r' c='ctest_test_send_receive_run'/>
-<use f='codebrowser/src/test/test_websocket.c' l='53' u='r' c='ctest_test_send_receive_run'/>
-<use f='codebrowser/src/test/test_websocket.c' l='58' u='r' c='ctest_test_send_data_run'/>
-<use f='codebrowser/src/test/test_websocket.c' l='59' u='r' c='ctest_test_send_data_run'/>
-<use f='codebrowser/src/test/test_websocket.c' l='65' u='r' c='ctest_test_send_frame_run'/>
-<use f='codebrowser/src/test/test_websocket.c' l='66' u='r' c='ctest_test_send_frame_run'/>
-<use f='codebrowser/src/test/test_websocket.c' l='78' u='r' c='ctest_test_message_run'/>
+<dec f='codebrowser/src/test/test_rpc_client.c' l='13' type='vws_cnx *'/>
+<use f='codebrowser/src/test/test_rpc_client.c' l='19' u='w' c='ctest_test_setup'/>
+<use f='codebrowser/src/test/test_rpc_client.c' l='20' u='r' c='ctest_test_setup'/>
+<use f='codebrowser/src/test/test_rpc_client.c' l='21' u='r' c='ctest_test_setup'/>
+<use f='codebrowser/src/test/test_rpc_client.c' l='23' u='r' c='ctest_test_setup'/>
+<use f='codebrowser/src/test/test_rpc_client.c' l='31' u='r' c='ctest_test_teardown'/>
+<offset>0</offset>
+<dec f='codebrowser/src/test/test_websocket.c' l='35' type='vws_cnx *'/>
+<use f='codebrowser/src/test/test_websocket.c' l='40' u='w' c='ctest_test_setup'/>
+<use f='codebrowser/src/test/test_websocket.c' l='41' u='r' c='ctest_test_setup'/>
+<use f='codebrowser/src/test/test_websocket.c' l='42' u='r' c='ctest_test_setup'/>
+<use f='codebrowser/src/test/test_websocket.c' l='50' u='r' c='ctest_test_teardown'/>
+<use f='codebrowser/src/test/test_websocket.c' l='55' u='r' c='ctest_test_send_receive_run'/>
+<use f='codebrowser/src/test/test_websocket.c' l='56' u='r' c='ctest_test_send_receive_run'/>
+<use f='codebrowser/src/test/test_websocket.c' l='61' u='r' c='ctest_test_send_data_run'/>
+<use f='codebrowser/src/test/test_websocket.c' l='62' u='r' c='ctest_test_send_data_run'/>
+<use f='codebrowser/src/test/test_websocket.c' l='68' u='r' c='ctest_test_send_frame_run'/>
+<use f='codebrowser/src/test/test_websocket.c' l='69' u='r' c='ctest_test_send_frame_run'/>
 <use f='codebrowser/src/test/test_websocket.c' l='81' u='r' c='ctest_test_message_run'/>
+<use f='codebrowser/src/test/test_websocket.c' l='84' u='r' c='ctest_test_message_run'/>
 <offset>64</offset>
