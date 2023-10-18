92d2e41bb42f5d7e74efcafb369ce6b3d822bf2c
vrtql <vrtql@vrtql.com>
2023-10-18 11:00:07 -0500

    update

--- ws-code-doc/root/refs/vws_socket..hs
+++ ws-code-doc/root/refs/vws_socket..hs
@@ -1,7 +1,7 @@
 <dec f='codebrowser/src/socket.h' l='48' type='vws_socket_hs'/>
 <offset>320</offset>
 <doc f='codebrowser/src/socket.h' l='47'>/**&lt; User-defined handshake function to be called on connect */</doc>
-<use f='codebrowser/src/socket.c' l='87' u='w' c='vws_socket_ctor'/>
-<use f='codebrowser/src/socket.c' l='354' u='r' c='vws_socket_connect'/>
-<use f='codebrowser/src/socket.c' l='356' u='r' c='vws_socket_connect'/>
+<use f='codebrowser/src/socket.c' l='77' u='w' c='vws_socket_ctor'/>
+<use f='codebrowser/src/socket.c' l='344' u='r' c='vws_socket_connect'/>
+<use f='codebrowser/src/socket.c' l='346' u='r' c='vws_socket_connect'/>
 <use f='codebrowser/src/websocket.c' l='301' u='w' c='vws_cnx_new'/>
